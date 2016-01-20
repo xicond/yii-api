84284bea4397889615d7d89d26ca3d5f8b18b6bf
root <root@debian-jessie.vagrantup.com>
2016-01-20 08:56:37 +0000

    revise structure

--- app/config/main.php
+++ app/config/main.php
@@ -44,8 +44,8 @@ return [
 //                'enableStrictParsing' => true,
 //                'showScriptName' => false,
             'rules' => [
-                ['class' => 'yii\rest\UrlRule', 'controller' => ['v1/user', 'v1/post']],
-//                ['class' => 'yii\rest\UrlRule', 'controller' => ['v2/user', 'v2/post']],
+                ['class' => 'yii\rest\UrlRule', 'controller' => ['user', 'post']],
+//                ['class' => 'app\components\RestUrlRule', 'controller' => ['v2/user', 'v2/post']],
             ],
         ],
         'request' => [
